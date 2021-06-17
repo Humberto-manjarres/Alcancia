bf6030add6dbb29871f63d235a374f723f8ec667
Humberto manjarres <totopercusion@gmail.com>
2021-06-17 18:55:38 -0500

    primer commit proyecto mutualSer

--- src/java/controlador/Controlador.java
+++ src/java/controlador/Controlador.java
@@ -14,7 +14,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import jdk.nashorn.internal.ir.BreakNode;
-import logica.Monedas;
+import logica.LogicaMoneda;
+import maestros.Monedas;
 
 /**
  *
@@ -22,140 +23,71 @@ import logica.Monedas;
  */
 public class Controlador extends HttpServlet {
 
-    Monedas monedas;
+    
     List<Monedas> listaMonedas = new ArrayList<>();
+    LogicaMoneda logicaMoneda = new LogicaMoneda();
+   
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        int acumCant = 0, acumMonto = 0;
         int acumCantidad1 = 0, acumCantidad2 = 0, acumCantidad3 = 0, acumCantidad4 = 0, acumCantidad5 = 0;
         int acumValor1 = 0, acumValor2 = 0, acumValor3 = 0, acumValor4 = 0, acumValor5 = 0;
-        int totalCantidad = 0, totalValor = 0;
         String accion = request.getParameter("accion");
-        if (!listaMonedas.isEmpty()) {
-            /* mantener la tabla con resultados de todas las monedas*/
-            for (Monedas listaMoneda : listaMonedas) {
-                if (listaMoneda.getMoneda().equals("50")) {
-                    acumCantidad1 = acumCantidad1 + listaMoneda.getCantidad();
-                    acumValor1 = acumValor1 + listaMoneda.getValor();
-                } else if (listaMoneda.getMoneda().equals("100")) {
-                    acumCantidad2 = acumCantidad2 + listaMoneda.getCantidad();
-                    acumValor2 = acumValor2 + listaMoneda.getValor();
-                } else if (listaMoneda.getMoneda().equals("200")) {
-                    acumCantidad3 = acumCantidad3 + listaMoneda.getCantidad();
-                    acumValor3 = acumValor3 + listaMoneda.getValor();
-                } else if (listaMoneda.getMoneda().equals("500")) {
-                    acumCantidad4 = acumCantidad4 + listaMoneda.getCantidad();
-                    acumValor4 = acumValor4 + listaMoneda.getValor();
-                } else if (listaMoneda.getMoneda().equals("1000")) {
-                    acumCantidad5 = acumCantidad5 + listaMoneda.getCantidad();
-                    acumValor5 = acumValor5 + listaMoneda.getValor();
-                }
-            }
-        }else{
-            
-        }
 
         if (accion.equals("Agregar")) {
-            listaMonedas.clear();
-            if (!request.getParameter("txtMoneda1").equals("")) {
-                int moneda1 = Integer.parseInt(request.getParameter("txtMoneda1"));
-                monedas = new Monedas();
-                monedas.setMoneda("50");
-                monedas.setCantidad(moneda1 + acumCantidad1);
-                monedas.setValor((moneda1 * 50) + acumValor1);
-                listaMonedas.add(monedas);
-            } else {
-                monedas = new Monedas();
-                monedas.setMoneda("50");
-                monedas.setCantidad(acumCantidad1);
-                monedas.setValor(acumValor1);
-                listaMonedas.add(monedas);
-            }
 
-            if (!request.getParameter("txtMoneda2").equals("")) {
-                int moneda2 = Integer.parseInt(request.getParameter("txtMoneda2"));
-                monedas = new Monedas();
-                monedas.setMoneda("100");
-                monedas.setCantidad(moneda2 + acumCantidad2);
-                monedas.setValor((moneda2 * 100) + acumValor2);
-                listaMonedas.add(monedas);
-            } else {
-                monedas = new Monedas();
-                monedas.setMoneda("100");
-                monedas.setCantidad(acumCantidad2);
-                monedas.setValor(acumValor2);
-                listaMonedas.add(monedas);
-            }
+            String moneda1 = request.getParameter("txtMoneda1");
+            String moneda2 = request.getParameter("txtMoneda2");
+            String moneda3 = request.getParameter("txtMoneda3");
+            String moneda4 = request.getParameter("txtMoneda4");
+            String moneda5 = request.getParameter("txtMoneda5");
+            listaMonedas = logicaMoneda.getMoneda(moneda1, moneda2, moneda3, moneda4, moneda5);
+            
+            if (!listaMonedas.isEmpty()) {
+                /* mantener la tabla con resultados de todas las monedas*/
+                for (Monedas listaMoneda : listaMonedas) {
+                    if (listaMoneda.getMoneda().equals("50")) {
+                        acumCantidad1 = acumCantidad1 + listaMoneda.getCantidad();
+                        acumValor1 = acumValor1 + listaMoneda.getValor();
+                        request.setAttribute("total_50", acumValor1);
+                        request.setAttribute("cantidad_50", acumCantidad1);
+                    } else if (listaMoneda.getMoneda().equals("100")) {
+                        acumCantidad2 = acumCantidad2 + listaMoneda.getCantidad();
+                        acumValor2 = acumValor2 + listaMoneda.getValor();
+                        request.setAttribute("total_100", acumValor2);
+                        request.setAttribute("cantidad_100", acumCantidad2);
+                    } else if (listaMoneda.getMoneda().equals("200")) {
+                        acumCantidad3 = acumCantidad3 + listaMoneda.getCantidad();
+                        acumValor3 = acumValor3 + listaMoneda.getValor();
+                        request.setAttribute("total_200", acumValor3);
+                        request.setAttribute("cantidad_200", acumCantidad3);
+                    } else if (listaMoneda.getMoneda().equals("500")) {
+                        acumCantidad4 = acumCantidad4 + listaMoneda.getCantidad();
+                        acumValor4 = acumValor4 + listaMoneda.getValor();
+                        request.setAttribute("total_500", acumValor4);
+                        request.setAttribute("cantidad_500", acumCantidad4);
+                    } else if (listaMoneda.getMoneda().equals("1000")) {
+                        acumCantidad5 = acumCantidad5 + listaMoneda.getCantidad();
+                        acumValor5 = acumValor5 + listaMoneda.getValor();
+                        request.setAttribute("total_1000", acumValor5);
+                        request.setAttribute("cantidad_1000", acumCantidad5);
+                    }else{
+                        request.setAttribute("mensaje", "Moneda no valida");
+                    }
+                }
+                int cantidadMonedas = acumCantidad1 + acumCantidad2 + acumCantidad3 + acumCantidad4 + acumCantidad5;
+                int valorTotal = acumValor1 + acumValor2 + acumValor3 + acumValor4 + acumValor5;
 
-            if (!request.getParameter("txtMoneda3").equals("")) {
-                int moneda3 = Integer.parseInt(request.getParameter("txtMoneda3"));
-                monedas = new Monedas();
-                monedas.setMoneda("200");
-                monedas.setCantidad(moneda3 + acumCantidad3);
-                monedas.setValor((moneda3 * 200) + acumValor3);
-                listaMonedas.add(monedas);
+                request.setAttribute("cantidad_monedas", cantidadMonedas);
+                request.setAttribute("valor_total", valorTotal);
             } else {
-                monedas = new Monedas();
-                monedas.setMoneda("200");
-                monedas.setCantidad(acumCantidad3);
-                monedas.setValor(acumValor3);
-                listaMonedas.add(monedas);
+                request.setAttribute("mensaje", "Debe Insertar al menos una moneda!");
             }
-
-            if (!request.getParameter("txtMoneda4").equals("")) {
-                int moneda4 = Integer.parseInt(request.getParameter("txtMoneda4"));
-                monedas = new Monedas();
-                monedas.setMoneda("500");
-                monedas.setCantidad(moneda4 + acumCantidad4);
-                monedas.setValor((moneda4 * 500) + acumValor4);
-                listaMonedas.add(monedas);
-            } else {
-                monedas = new Monedas();
-                monedas.setMoneda("500");
-                monedas.setCantidad(acumCantidad4);
-                monedas.setValor(acumValor4);
-                listaMonedas.add(monedas);
-            }
-
-            if (!request.getParameter("txtMoneda5").equals("")) {
-                int moneda5 = Integer.parseInt(request.getParameter("txtMoneda5"));
-                monedas = new Monedas();
-                monedas.setMoneda("1000");
-                monedas.setCantidad(moneda5 + acumCantidad5);
-                monedas.setValor((moneda5 * 1000) + acumValor5);
-                listaMonedas.add(monedas);
-            } else {
-                monedas = new Monedas();
-                monedas.setMoneda("1000");
-                monedas.setCantidad(acumCantidad5);
-                monedas.setValor(acumValor5);
-                listaMonedas.add(monedas);
-            }
-            request.setAttribute("lista_monedas", listaMonedas);
-
-            for (Monedas listaMoneda : listaMonedas) {
-                acumCant = acumCant + listaMoneda.getCantidad();
-                acumMonto = acumMonto + listaMoneda.getValor();
-            }
-            request.setAttribute("totalCantidad", acumCant);
-            request.setAttribute("totalValor", acumMonto);
 
         } else if (accion.equals("Limpiar")) {
             System.out.println("Limpiar");
-            request.setAttribute("lista_monedas", listaMonedas);
-            request.setAttribute("totalCantidad", acumCant);
-            request.setAttribute("totalValor", acumMonto);
+            
         } else if (accion.equals("Vaciar Alcancia")) {
             listaMonedas.clear();
         }
